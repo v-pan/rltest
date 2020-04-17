e9595d1cd992cfb43f5594ba7ffc3d00c4c055f1
paranoidcake <victorpanayotov@gmail.com>
2020-04-17 01:30:00 +0100

    Streamline Task interface

--- src/main/kotlin/data/Task.kt
+++ src/main/kotlin/data/Task.kt
@@ -3,8 +3,5 @@ package data
 import MDPRLA
 
 interface Task {
-//    val actionTable: HashMap<Any, List<KFunction<State>>> // TODO: Replace with value -> action map? Create Value interface?
     val rla: MDPRLA
-
-    fun start()
 }
